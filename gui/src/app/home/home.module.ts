37f5b7180552e746162b8ac43345eeb02a92dd73
rojefferson <jrm@cin.ufpe.br>
2018-06-24 23:09:22 -0300

    Evento para validar login e primeiras configurações  do servidor

--- gui/src/app/home/home.module.ts
+++ gui/src/app/home/home.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,ReactiveFormsModule
    
   ],
   declarations: [
